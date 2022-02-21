7cf7200a05f558a48b0586a33aa4719fb56acc2c
Dupomi <e238499@gmail.com>
2022-02-21 17:59:06 +0800

    add file

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,28 @@
+<template>
+<div id="nav">
+    <router-link to="/products">產品清單</router-link> |
+    <router-link to="/checkout">結帳(共有 {{ currentQuantity }} 個商品)</router-link>
+</div>
+</template>
+<script>
+export default {
+    computedQuantity(){
+        return this.getters.currentQuamtity
+    }
+}
+</script>
+
+<style scoped>
+    #nav{
+        padding:30px;
+        text-align: center;
+        background: beige;
+    }
+    #nav a{
+        font-weight: bold;
+        color: #2c3e50;
+    }
+    #nav a.router-link-exact-active{
+        color: #42b983;
+    }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+
+import { createRouter, createWebHistory } from 'vue-router'
+import ProductsView from '../views/ProductsView.vue'
+import ProductView from '../views/ProductView.vue'
+const routes = [
+  {
+    path: '/products',
+    name: 'products',
+    component: ProductsView
+  },
+  {
+    path: '/products/:productId',
+    name: 'product',
+    component: ProductView
+  }
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
